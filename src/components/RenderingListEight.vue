714a8a6f9c19a5d86224f8dc3d543a7739cf92f7
MateuszKar2 <system505@protonmail.com>
2024-06-02 18:56:34 +0200

    add file

--- src/components/RenderingListEight.vue
+++ src/components/RenderingListEight.vue
@@ -1,69 +1,83 @@
 <!-- v-for- renderowanie listy -->
+<!--nie powiniśmy mieszać warunkowania z v-if-->
 
 <template>
     <div  class="candidates">
-        <div v-for="(candidate,index) in candidates" :key="candidate.id" class="candidates__card"><!--musi być unkalny klucz w postaci id-->
+        <div v-for="(candidate,index) in candidateWithParty" 
+            :key="candidate.id"
+            class="candidates__card"
+            > <!--musi być unkalny klucz w postaci id-->
             <p>Candidate: {{ index + 1 }}</p>
             <p>Name: {{ candidate.firstName }} {{ candidate.lastName }}</p>
-            <p>Age: {{ candidates.age }}</p>
-            <p>Party: {{ candidate.party }}</p>
-            <div>
+            <p>Age: {{ candidate.age }}</p>
+            <p v-if="candidate.party">Party: {{ candidate.party }}</p>
+            <div v-if="candidate.opinionPool"> 
                 Opinion Pool:
-                
+                <progress max="100" :value="candidate.opinionPool"></progress> 
+                {{ candidate.opinionPool}}%
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {ref} from 'vue';
+    import {ref, computed} from 'vue';
 
     export default {
         name: "RenderingListEight",
         setup() {
-            const candidates = ([
+            const candidateWithParty = computed(() => candidates.value.filter(({party}) => party))
+            const candidates = ref([
                 {
                     id: 1,
                     firstName: 'Zuzanna',
-                    lastName: 'Janiszewska',
+                    lastName: 'Janik',
                     age: 25,
                     party: 'Lewica',
-                    opinionPool: 203,
-                }
+                    opinionPool: 20.3,
+                },
                 {
                     id: 2,
                     firstName: 'Agnieszka',
                     lastName: 'Kubik',
                     age: 27,
-                    party: 'PIS',
-                    opinionPool: 149,
-                }
+                    party: '',
+                    opinionPool: 14.9,
+                },
                 {
-                    id: 1,
+                    id: 3,
                     firstName: 'Natalia',
                     lastName: 'Laskowska',
                     age: 43,
                     party: 'PO',
-                    opinionPool: 271,
-                }
+                    opinionPool: 27.1,
+                },
                 {
-                    id: 1,
+                    id: 4,
                     firstName: 'Michał',
                     lastName: 'Kubik',
                     age: 21,
                     party: 'Konfederacja',
-                    opinionPool: 42,
-                }
+                    opinionPool: 4.2,
+                },
                 {
-                    id: 1,
+                    id: 5,
                     firstName: 'Adam',
                     lastName: 'Borkowski',
                     age: 26,
                     party: 'Lewica',
-                    opinionPool: 203,
+                    opinionPool: 11.9,
+                },
+                {
+                    id: 6,
+                    firstName: 'Paweył',
+                    lastName: 'Dobosz',
+                    age: 47,
+                    party: 'PIS',
+                    opinionPool: 35.9,
                 }
             ])
-                    return {candidates}
+                    return {candidates, candidateWithParty}
         }
     }
 </script>
