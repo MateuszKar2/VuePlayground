c5ef303e0baa17f52f7946b4931512b9cc337d66
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:39:37 +0200

    add file

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,48 @@
 <template>
   <div id="app">
-    <MyAwsomeComponent/>
-    <RefTwoComponent/>
-    <ReactiveThreeComponent/>
+    <MyAwsomeOne/>
+    <RefTwo/>
+    <ReactiveThree/>
     <ComputedPropertiesFour/>
     <WatchFive/>
-    <ConditionalRendering/>
+    <ConditionalRenderingSix/>
     <BindSeven/>
     <RenderingListEight/>
+    <LifecycleHooksTen/>
+    <SlotyEleven/>
+    <SuspenseTwelve/>
   </div>
 </template>
 
 <script>
-// import MyAwsomeComponent from './components/MyAwsomeComponent.vue';
-// import RefTwoComponent from './components/RefTwoComponent.vue';
-// import ReactiveThreeComponent from './components/ReactiveThreeComponent';
+// import MyAwsomeOne from './components/MyAwsomeOne.vue';
+// import RefTwo from './components/RefTwo.vue';
+// import ReactiveThree from './components/ReactiveThree';
 // import ComputedPropertiesFour from './components/ComputedPropertiesFour';
 // import WatchFive from './components/WatchFive.vue';
-// import ConditionalRendering from './components/ConditionalRenderingSix.vue';
+// import ConditionalRenderingSix from './components/ConditionalRenderingSix.vue';
 // import BindSeven from './components/BindSeven.vue';
-import RenderingListEight from './components/RenderingListEight'
+// import RenderingListEight from './components/RenderingListEight'
+// import LifecycleHooksTen from './components/LifecycleHooksTen.vue';
+// import SlotyEleven from './components/SlotyEleven.vue';
+import { SuspenseTwelve } from './components/SuspenseTwelve.vue'
+
 export default {
   name: 'App',
 
   components: {
 
-    // MyAwsomeComponent,
-    // RefTwoComponent,
-    // ReactiveThreeComponent,
+    // MyAwsomeOne,
+    // RefTwo,
+    // ReactiveThree,
     // ComputedPropertiesFour,
     // WatchFive,
-    // ConditionalRendering,
+    // ConditionalRenderingSix,
     // BindSeven,
-    RenderingListEight,
+    // RenderingListEight,
+    // LifecycleHooksTen,
+    // SlotyEleven
+    SuspenseTwelve
 
   }
 }
