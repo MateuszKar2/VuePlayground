f5a55d22f5bd130e850d7f27e41b4e89006431df
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:39:51 +0200

    add file

---
+++ src/components/AuxiliaryComponents/StarShipList.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <section>
+        <h1>Ships List</h1>
+        <StarShipItem v-for="(ship, index) in ships" :key="index" :ship="ship"/>
+    </section>
+
+</template>
+
+<script>
+import StarShipItem from './StarShipItem'
+import ref from 'vue'
+
+    export default {
+        name: "StarShipList",
+        components: {
+            StarShipItem
+        },
+
+        async setup() {
+            const ships = ref(null)
+            const response = await fetch('https://swapi.dev/api/starships/') //będziemy asynchronicznie łądować dane
+            const parsedResponse = await response.json() // sparsować dane
+
+            ships.value = [...parsedResponse.results]
+
+        return { ships}
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
