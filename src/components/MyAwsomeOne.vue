ee80736a0b176e9b5c73406565d5b841fd44649c
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:40:14 +0200

    add file

---
+++ src/components/MyAwsomeOne.vue
@@ -0,0 +1,29 @@
+
+<!-- 1.funkcje -->
+ <template>
+  <div>
+    <button @click="sayHello">Say hello</button> <!-- v-on:click === @click -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyAwsomeOne',
+  setup(){
+    function sayHello() {
+      const welcomeText = 'Siema, co tam?'
+      console.log(addExclamationText(welcomeText));
+    }
+    function addExclamationText(text) {
+        return `${text}!`
+    }
+    return {sayHello}
+  }
+}
+</script>
+<!-- cd vue-project
+npm run serve -->
+
+
+
+
