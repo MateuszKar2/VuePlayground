92c5bfb35dedd71ec8e34cb1ef1be9c7825f3a43
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:40:34 +0200

    add file

---
+++ src/components/SlotyEleven.vue
@@ -0,0 +1,50 @@
+<!--sloty-->
+<!--przekazywania np. logo-->
+<template>
+    <div class="container">
+      <AwsomeButton :text="'hello'">
+            <div class="square"></div>
+      </AwsomeButton>
+      <!-- <AwsomeButton :text="'hello'">
+        <template #text>
+            <h1>
+                huraaaa
+            </h1>
+        </template>
+        <template #image>
+            <img src="../assets/logo.png" alt="logo" class="image"/>
+        </template>
+      </AwsomeButton> -->
+    </div>
+  </template>
+  
+  <script>
+import AwsomeButton from './AuxiliaryComponents/AwsomeButton';
+
+  export default {
+    name: 'SlotyEleven',
+    components: {
+        AwsomeButton
+    }
+  }
+  </script>
+
+    <style lang="scss">
+        .container {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .image {
+            width: 20px;
+            height: 20px;
+        }
+        .square {
+            width: 40px;
+            height: 40px;
+            background-color: red;
+        }
+
+    </style>
