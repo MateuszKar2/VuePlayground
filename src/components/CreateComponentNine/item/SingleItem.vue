a11cc3032849a3e69e9ea19bb9bf6c007cd46fda
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:40:00 +0200

    add file

--- src/components/CreateComponentNine/item/SingleItem.vue
+++ src/components/CreateComponentNine/item/SingleItem.vue
@@ -4,11 +4,11 @@
 
 
 <template>
-    <div  class="single-item"
-            > <!--musi być unkalny klucz w postaci id-->
+    <div  class="single-item"> 
+        <!--musi być unkalny klucz w postaci id-->
             <p>Candidate: {{ index + 1 }}</p>
-            <p>Name: {{ item.firstName }} {{ item.lastName }}</p>
-            <p>Age: {{ item.age }}</p>
+            <p>Name: {{ firstName }} {{ lastName }}</p>
+            <p>Age: {{ ageWithYear }}</p>
             <p v-if="item.party">Party: {{ item.party }}</p>
             <div v-if="item.opinionPool"> 
                 Opinion Pool:
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { computed } from 'vue';
 
     export default {
         name: "SingleItem",
@@ -26,18 +27,37 @@
             item: {
                 type: Object,
                 default: () => ({}),
-            }
+            },
             index: {
                 type: Number,
                 default: 0,
-            }
+            },
+            firstName: {
+                type: String,
+                default: '',
+            },
+            lastName: {
+                type: String,
+                default: '',
+                //required: true,//pole jest wymagane
+            },
+            age: {
+                type: Number,
+                default: 0,
+            },
+        },
+        setup(props) {
+            const ageWithYear = computed(() => `${props.age} years`)
+
+            return { ageWithYear }
         }
     }
 </script>
-<style lang="scss" scoped/>
+<style lang="scss" scoped>//scoped - indywidualny identyfikator, style nie będą się powtarzać/klasa dotyczy tylko tego komponentu
 
-        .single-item  {
-        border: 1px solid black;
-        padding: 10px;
-    }
+         .single-item {
+            border: 1px solid black;
+            padding: 10px;
+         }
+
 </style>
