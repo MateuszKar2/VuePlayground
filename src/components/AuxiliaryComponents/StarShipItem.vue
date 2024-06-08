73a6f26685ee325af40d5376406e079ab2d5cb9a
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:39:47 +0200

    add file

---
+++ src/components/AuxiliaryComponents/StarShipItem.vue
@@ -0,0 +1,27 @@
+
+
+
+<template>
+    <section>
+        <p class="ship">{{ ship.name }}</p>
+    </section>
+
+</template>
+
+<script>
+    export default {
+        name: "StarShipItem",
+        props: {
+            ship: {
+                type: Object,
+                default: () => {}
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .ship {
+        color: #fff,
+    }
+</style>
