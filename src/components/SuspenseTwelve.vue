a9ebd69403790d47be6310b207a015e106d4e4a0
MateuszKar2 <system505@protonmail.com>
2024-06-08 11:40:38 +0200

    add file

---
+++ src/components/SuspenseTwelve.vue
@@ -0,0 +1,30 @@
+Suspense
+<template>
+    <div class="container">
+    <Suspense>
+        <template #default>
+            <StarShipList></StarShipList>
+        </template>
+    </Suspense>
+    </div>
+<script>
+import StarShipList from './AuxiliaryComponents/StarShipList.vue';
+
+
+    export default {
+        name: "SuspenseTwelve",
+        components: {StarShipList, AppLoader}
+    };
+</script>
+
+<style lang="scss">
+.container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #000;
+    color: #fcd711
+}
+</style>
